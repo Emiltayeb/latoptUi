7bf3efdfff65e71d16a596a62c33bdb16e51bc54
emil tayeb <emiltayeb@gmail.com>
2020-03-20 13:08:50 +0200

    itnial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const navbar_active = () => {
+  const hambuerger = document.querySelector('.hamburger')
+  const navbar = document.querySelector('.nav-links')
+
+  const links = document.querySelectorAll('.nav-links li')
+  hambuerger.addEventListener('click', () => {
+    navbar.classList.toggle('navbar-show')
+
+
+    links.forEach((link, index) => {
+      console.log(link);
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `fade-in ${(1/index)+0.6}s ease-in`
+      }
+
+    })
+  })
+}
+
+
+
+navbar_active();
